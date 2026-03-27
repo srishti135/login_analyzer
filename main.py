831f6ec8de858b701f5b7535875d30756635bb58
Srishti <srishtijain906@gmail.com>
2026-03-27 17:45:00 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-from file_handler import load_logs
+from file_handler import group_by_user, load_logs
 
 df = load_logs("logs/login_logs.csv")
 print(df)
+df1 = group_by_user(df)
+print(df1)
+'''
+for name, group in df1:
+    print(name)
+    print(group)
+    print("---")
+'''
+from login_analyzer import LoginAnalyzer
+analyzer = LoginAnalyzer(df1)
+analyzer.check_failed_logins()
