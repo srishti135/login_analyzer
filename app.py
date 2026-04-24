4b5bad785c6e12f0e047ea5b8d4d2e2f86f62559
Srishti <srishtijain906@gmail.com>
2026-04-24 17:20:53 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -4,9 +4,6 @@ from login_analyzer import LoginAnalyzer
 from visualizer import plot_failed_logins, plot_suspicious_ratio, plot_login_timeline
 
 # Generate graphs when app starts
-plot_failed_logins()
-plot_suspicious_ratio()
-plot_login_timeline()
 app = Flask(__name__)
 
 @app.route("/")
@@ -44,6 +41,11 @@ def dashboard():
 def admin_login():
     password = request.form["password"]
     if password == "admin123":
+        # Generate graphs fresh
+        plot_failed_logins()
+        plot_suspicious_ratio()
+        plot_login_timeline()
+        
         df = load_logs("logs/login_logs.csv")
         df1 = group_by_user(df)
         analyzer = LoginAnalyzer(df1)
@@ -53,7 +55,7 @@ def admin_login():
         return render_template("dashboard.html", all_findings=all_findings)
     else:
         return render_template("admin_login.html", error="Wrong password!")
-    if __name__ == "__main__":
-        import os
-        port = int(os.environ.get("PORT", 5000))
-        app.run(host="0.0.0.0", port=port)
+        if __name__ == "__main__":
+            import os
+            port = int(os.environ.get("PORT", 5000))
+            app.run(host="0.0.0.0", port=port)
