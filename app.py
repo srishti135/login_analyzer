5782076a0cdc3dc02f489817c6d98ee68dab728b
Srishti <srishtijain906@gmail.com>
2026-04-24 17:25:54 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template, request
 from file_handler import load_logs, group_by_user
 from login_analyzer import LoginAnalyzer
-from visualizer import plot_failed_logins, plot_suspicious_ratio, plot_login_timeline
+import os
 
-# Generate graphs when app starts
 app = Flask(__name__)
 
 @app.route("/")
@@ -13,26 +12,20 @@ def home():
 @app.route("/check", methods=["POST"])
 def check():
     username = request.form["username"]
-
     df = load_logs("logs/login_logs.csv")
     df1 = group_by_user(df)
     analyzer = LoginAnalyzer(df1)
-
     findings = analyzer.check_failed_logins()
     findings += analyzer.check_suspicious_hours()
     findings += analyzer.check_multiple_ips()
-
     flagged_users = [f.split()[1] for f in findings]
-
-    # Get this user's specific stats
     user_findings = [f for f in findings if username in f]
-
     if username in flagged_users:
         result = f"Access Denied — {username} is flagged as suspicious!"
     else:
         result = f"Access Granted — Welcome {username}!"
-
     return render_template("result.html", result=result, username=username, user_findings=user_findings)
+
 @app.route("/dashboard")
 def dashboard():
     return render_template("admin_login.html")
@@ -41,11 +34,10 @@ def dashboard():
 def admin_login():
     password = request.form["password"]
     if password == "admin123":
-        # Generate graphs fresh
+        from visualizer import plot_failed_logins, plot_suspicious_ratio, plot_login_timeline
         plot_failed_logins()
         plot_suspicious_ratio()
         plot_login_timeline()
-        
         df = load_logs("logs/login_logs.csv")
         df1 = group_by_user(df)
         analyzer = LoginAnalyzer(df1)
@@ -55,7 +47,7 @@ def admin_login():
         return render_template("dashboard.html", all_findings=all_findings)
     else:
         return render_template("admin_login.html", error="Wrong password!")
-        if __name__ == "__main__":
-            import os
-            port = int(os.environ.get("PORT", 5000))
-            app.run(host="0.0.0.0", port=port)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
